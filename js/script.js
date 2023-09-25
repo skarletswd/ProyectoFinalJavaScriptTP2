9b25de85eaf6b02097881ce3454166082ec533a0
Andrea Lopez <skarletswdev@gmail.com>
2023-09-25 09:03:08 -0300

    menu dinamico paquetesTuristicos

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
-const hSecund = document.createElement("h4");
+const paquetesTuristicos = [
+    "Cerro Chapelco: Amantes de la nieve",
+    "Navegación: Escapada medio día",
+    "Volcán Lanin: Lomás popular",
+    "Península Yuco: Paraíso terrenal",
+    "Rafting: Adictos a la adrenalina",
+    "Montaña: En bici medio día"
+];
 
-hSecund.textContent = "Título dinámico";
-hSecund.setAttribute("id", "hSecund");
+console.log(paquetesTuristicos)
 
-console.log(hSecund);
-
-const liProdH = document.querySelector("li");
-liProdH.appendChild(hSecund);
-
-
-
-const pSecund = document.createElement("p");
-
-pSecund.textContent = "Párrafo dinámico";
-pSecund.setAttribute("id", "pSecund");
-
-console.log(pSecund);
-
-const liProdP = document.querySelector("li");
-liProdP.appendChild(pSecund);
+const menuTur = document.querySelector(".paqsTur");
+for(let i=0; i<paquetesTuristicos.length; i++) {
+    menuTur.innerHTML += `<li><a href="#">${paquetesTuristicos[i]}</a></li>` 
+}
 
 
-const aPrincipal = document.createElement("a");
+// const hSecund = document.createElement("h4");
 
-aPrincipal.textContent = "IrD";
-aPrincipal.setAttribute("id", "aPrincipal");
+// hSecund.textContent = "Título dinámico";
+// hSecund.setAttribute("id", "hSecund");
 
-console.log(aPrincipal);
+// console.log(hSecund);
 
-const liProdA = document.querySelector("li");
-liProdA.appendChild(aPrincipal);
+// const liProdH = document.querySelector("li");
+// liProdH.appendChild(hSecund);
+
+
+
+// const pSecund = document.createElement("p");
+
+// pSecund.textContent = "Párrafo dinámico";
+// pSecund.setAttribute("id", "pSecund");
+
+// console.log(pSecund);
+
+// const liProdP = document.querySelector("li");
+// liProdP.appendChild(pSecund);
+
+
+// const aPrincipal = document.createElement("a");
+
+// aPrincipal.textContent = "IrD";
+// aPrincipal.setAttribute("id", "aPrincipal");
+
+// console.log(aPrincipal);
+
+// const liProdA = document.querySelector("li");
+// liProdA.appendChild(aPrincipal);
